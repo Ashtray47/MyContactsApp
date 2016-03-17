554dd717225259103427a42a7c0ed50fe13f215a
Mark Sau <mark_sau@apple.com>
2016-03-17 11:19:49 -0700

    initial commit

---
+++ data/contacts.js
@@ -0,0 +1,24 @@
+
+var data =[
+	{
+		id: "1",
+		firstname: "Dave",
+		lastname: "Kay",
+		birthday: "11/11/1969",
+		company: "J&K",
+		email: "dk@email.com",
+		phone: "555-555-1234"
+	},
+	{
+		id: "2",
+		firstname: "Daniel",
+		lastname: "Berry",
+		birthday: "07/06/1980",
+		company: "Next Tech",
+		email: "db@email.com",
+		phone: "415-777-4321"
+	}
+]
+
+
+module.exports = data;
